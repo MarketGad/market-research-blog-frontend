5065085b0ff860a3d52604f1bb7d652166f10594
Ajitesh13 <aji.yash13@gmail.com>
2020-07-19 18:55:35 +0530

    full stop deleted

--- src/components/Newpost.js
+++ src/components/Newpost.js
@@ -5,11 +5,11 @@ const Newpost = () => {
         <div className="newPost">
             <div className="row">
                 <div className="col s12 m6 l6 center tag1">
-                    <div className="tagline1">59% Startups fail due to lack of Market Research.</div>
+                    <div className="tagline1">59% Startups fail due to lack of Market Research</div>
                 </div>
                 <div className="col s12 m6 l6 center tag2">
                     <div className="tagline1">
-                        Apple, Amazon, Disney & Google all started from a single garage.
+                        Apple, Amazon, Disney & Google all started from a single garage
                     </div>
                 </div>
             </div>
