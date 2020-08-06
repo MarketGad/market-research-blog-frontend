d5ff81927f756f414727049fe85123c9ae388d51
Rashmi Ranjan Das <62706300+WebDesign4New@users.noreply.github.com>
2020-08-06 12:12:41 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -27,8 +27,8 @@ class Home extends Component {
                 <Slider />
               
                 <div className="tagline center">
-                <h4 className="seo-tag1">59% of startups fail due to lack of Market Research</h4>
-                    <h4 className="seo-tag1">The difference is: Will you do something about it?</h4>
+                <h3 className="seo-tag1">59% of startups fail due to lack of Market Research</h4>
+                    <h3 className="seo-tag1">The difference is: Will you do something about it?</h4>
                 </div>
                 <Newpost />
                 {/* <Review /> */}
