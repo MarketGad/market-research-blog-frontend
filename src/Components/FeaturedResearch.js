7127adf69e6895db774503597d595f63abe1db63
Rashmi Ranjan Das <62706300+WebDesign4New@users.noreply.github.com>
2020-08-18 23:29:24 +0530

    Article added to featured research

--- src/Components/FeaturedResearch.js
+++ src/Components/FeaturedResearch.js
@@ -32,14 +32,23 @@ const FeaturedResearch = () => {
 			authorLink: 'https://www.linkedin.com/in/shankh0407',
 			authorName: 'Shankhadeep Banerjee',
 			postBrief:
-				'The Indian Healthcare sector stands as the world’s fourth-largest employer. The country employs over 3,20,000 people all around the country, which is also estimated to cross a valuation of 40 million by the end of 2020. Gloablly it is one of the firstest growing...',
+				'The Indian Healthcare sector stands as the world’s fourth-largest employer. The country employs over 3,20,000 people all around the country, which is also estimated to cross a valuation of 40 million by the end of 2020. Gloablly it is one of the fastest growing...',
 			postLink: 'https://drive.google.com/file/d/1jGBTzrtWh8Bv6sVXHG_yMk0RJtn9Pzry/view?usp=sharing'
+		},
+		{
+			postName: 'Part II: Impact Of Covid-19 On Logistics Sector In India',
+			postImage:
+				'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1597767818/Articles%20Photos/Industry/Logistics%20Sector%20Of%20India%20Part%20II/2_2_mys3kv.jpg',
+			key: '4',
+			postBrief:
+				'Since   the   3rd   quarter   of   2019,   the   Indian   economy   is   facing   turbulences   due   to   fallingmanufacturing/industrial   production,   especially   in   auto   and   ancillary   sector,   rising   non-performing assets (NPAs) among banks and rising unemployment',
+			postLink: '/i1007'
 		}
 	];
 
 	const showArticles = articles.length ? (
 		articles.map((article) => {
-			if (article.key !== '2') {
+			if (article.key % 2 == 1) {
 				return (
 					<div className='row card-h'>
 						<div className='col l5 s12'>
