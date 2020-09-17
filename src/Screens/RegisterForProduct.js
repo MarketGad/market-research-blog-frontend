9cbb869cd24273b1ccda47fa8d658ce0b1ed6c8a
Saidatta Sahu <saidatta.sahu02@gmail.com>
2020-09-17 07:42:27 +0530

    add product fix

--- src/Screens/RegisterForProduct.js
+++ src/Screens/RegisterForProduct.js
@@ -112,7 +112,7 @@ export default function RegisterForProduct () {
 						if (error.message === 'Request failed with status code 413') {
 							alert('upload photo size should be less than 500kb');
 						} else {
-							alert('something went wrong');
+							alert('please login to continue');
 						}
 					}
 				);
