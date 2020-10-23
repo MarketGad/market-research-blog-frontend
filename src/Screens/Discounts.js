2479dc67b549b1d1486320916d66cb216aa0c2a4
Nutella Enthusiast <ritika.iyer2000@gmail.com>
2020-10-23 20:30:18 +0530

    inline css shifted

--- src/Screens/Discounts.js
+++ src/Screens/Discounts.js
@@ -7,59 +7,25 @@ import Footer from "../Components/Footer";
 const Discounts = () => {
   const [openBazaarErr, setOpenBazaarErr] = React.useState(false);
   return (
-    <div
-      style={{
-        backgroundImage:
-          "url(" +
-          "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1603347962/Theme/Background_gprpm8.jpg" +
-          ")",
-        /*backgroundPosition: "center",*/
-        backgroundSize: "100%",
-        backgroundRepeat: "no-repeat",
-      }}
-    >
+    <div className="bazaar-bkgrd">
       <div className="container">
         <div className="product-content center">
           {/*<h5>
           Burn your interaction points to join the exclusive club and earn
           discounts on your subscriptions
         </h5>*/}
-          <div
-            style={
-              {
-                /* borderRight:'2px solid #87879f' ,borderTopLeftRadius:'20px', borderBottomLeftRadius:'20px' */
-              }
-            }
-          >
+          <div className="logo-gold">
             <img
               width="7%"
               src={
                 "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1603348061/Theme/Golden_MG_Logo_oxwzlm.png"
               }
-              style={{ padding: "3% 0 2% 0" }}
             />
           </div>
-          <div
-            style={
-              {
-                /* ,borderTopRightRadius:'20px',borderBottomRightRadius:'20px'*/
-              }
-            }
-          >
-            {/*<div className='center' style={{ fontSize: '1.6em', fontFamily: 'Bahnschrift', color: 'darkblue', padding:'3% 7% 3% 5%' }}>
-						997 MG users are in the waiting list!
-					</div>*/}
-            <div
-              className="center"
-              style={{
-                fontSize: "1 em",
-                fontFamily: "Bahnschrift",
-                color: "white",
-                padding: "0% 25% 1% 25%",
-              }}
-            >
+          <div>
+            <div className="center" className="baz">
               Burn your interaction points to join the exclusive club and earn
-              discounts on your subscription..
+              discounts on your subscription
             </div>
             <img
               width="15%"
@@ -68,26 +34,13 @@ const Discounts = () => {
               }
             />
             <div className="center">
-              <div
-                className="center"
-                style={{
-                  fontSize: "1 em",
-                  fontFamily: "Bahnschrift",
-                  color: "white",
-
-                  padding: "0% 22% 0% 22%",
-                }}
-              >
+              <div className="center" className="baz">
                 Burn 1000 points to enter
               </div>
-              <div style={{ padding: "3px" }}>
+              <div>
                 <a
                   className="waves-effect waves-light btn-small pro-btn1"
-                  style={{
-                    backgroundColor: "white",
-                    color: "darkblue",
-                    padding: "0 2% 0 2%",
-                  }}
+                  className="enter-button"
                   onClick={() => {
                     setOpenBazaarErr(true);
                   }}
