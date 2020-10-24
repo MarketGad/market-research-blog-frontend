5d257d93001101f10d0026512c43d945d355309d
ahtrahdis7 <sidharthabiki@gmail.com>
2020-10-24 13:23:06 +0530

    added date wise display

--- src/Components/Joblist.js
+++ src/Components/Joblist.js
@@ -4,19 +4,88 @@ import JobsandInternshipcard from './JobsandInternshipcard';
 
 const Joblist = (props) => {
 
-	const showJobs = props.jobs.length ? (
-		props.jobs.map((job, index) => {
-			if (!/^https?:\/\//.test(job.jobLink)) {
-				let weblink = 'https://' + job.jobLink;
-				return <JobsandInternshipcard key={index} jobdetails={job} weblink={weblink} />;
-			} else return <JobsandInternshipcard key={index} jobdetails={job} weblink={job.jobLink} />;
+	const jobs = props.jobs;
+	jobs.sort(function(a, b){return b.createdAt > a.createdAt}); 
+	var DD_ARR = []
+
+	for(var i=0; i < jobs.length; ){
+
+		if(jobs[i].createdAt != undefined){
+
+			var date = jobs[i].createdAt.split('T')[0];
+			var arr = [{date: date}];
+
+			while(date === jobs[i].createdAt.split('T')[0]){
+				arr.push(jobs[i]);
+				i++;
+				if( i >= jobs.length) break;
+			}
+			DD_ARR.push(arr);
+
+		} else {
+			var arr = [{date : "OLDER"}]
+			while(i < jobs.length){
+				arr.push(jobs[i]);
+				i++;
+			}
+			DD_ARR.push(arr);
+		}
+
+	}
+
+	const DateCard = (prop) => {
+
+		return(
+			<div
+				style={{
+					// backgroundColor: ''
+				}}
+			>
+				<h5
+					style={{
+						backgroundColor: '#dbdbdb',
+						padding: 10
+					}}
+				>{prop.date}</h5>
+			</div>
+		)
+	}
+
+	const ShowJobsDateWise = DD_ARR.length ? (
+		DD_ARR.map((list, index) => {
+			const show = list.splice(1).map((job, index) => {
+				if (!/^https?:\/\//.test(job.jobLink)) {
+					let weblink = 'https://' + job.jobLink;
+					return <JobsandInternshipcard key={index} jobdetails={job} weblink={weblink} />;
+				} else {
+					return <JobsandInternshipcard key={index} jobdetails={job} weblink={job.jobLink} />;
+				}
+			});
+			return (
+				<div>
+					<DateCard date={list[0].date} />
+					{show}
+				</div>
+			)
 		})
-	) : (
+	): (
 		<div className='center'> Loading... </div>
 	);
+	
+
+	// const showJobs = props.jobs.length ? (
+	// 	props.jobs.map((job, index) => {
+	// 		if (!/^https?:\/\//.test(job.jobLink)) {
+	// 			let weblink = 'https://' + job.jobLink;
+	// 			return <JobsandInternshipcard key={index} jobdetails={job} weblink={weblink} />;
+	// 		} else return <JobsandInternshipcard key={index} jobdetails={job} weblink={job.jobLink} />;
+	// 	})
+	// ) : (
+	// 	<div className='center'> Loading... </div>
+	// );
 	return (
 		<div>
-			{props.jobs[0] && <div>{showJobs}</div>}
+			{props.jobs[0] && <div>{ShowJobsDateWise}</div>}
 			{props.jobs.length === 0 && (
 				<div>
 					<FadingLoader loadno={3} />
