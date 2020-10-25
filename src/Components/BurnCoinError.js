502e485f72647f774625017fe441693ea041a5d9
Saidatta Sahu <saidatta.sahu02@gmail.com>
2020-10-25 20:20:16 +0530

    burncoin error updated

--- src/Components/BurnCoinError.js
+++ src/Components/BurnCoinError.js
@@ -3,34 +3,32 @@ import { Link } from 'react-router-dom';
 
 const BurnCoinError = (props) => {
 	return (
-		<div>
-			<div className='row'>
-				<div className='col l6 s12'>
-					<p style={{ textAlign: 'center' }}>
-						<img
-							width='80%'
-							src={
-								'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1603256319/Theme/9_Icon_qdgix7.png'
-							}
-						/>
-					</p>
+		<div className='row' style={{ margin: '0' }}>
+			<div className='col l6 s12 divider-popup'>
+				<div className='center'>
+					<img
+						width='80%'
+						src={
+							'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1603256319/Theme/9_Icon_qdgix7.png'
+						}
+					/>
 				</div>
-				<div className='col l6 s12'>
-					<div className='center' className='burncoinerr1'>
-						997 MG users are in the waiting list!
-					</div>
-					<div className='center' className='burncoinerr1'>
-						Burn your interaction points to join the exclusive discounts on the products.
+			</div>
+			<div className='col l6 s12' style={{ paddingTop: '15px' }}>
+				<div className='center' className='burncoinerr1'>
+					997 MG users are in the waiting list!
+				</div>
+				<div className='center' className='burncoinerr1'>
+					Burn your interaction points to join the exclusive discounts on the products.
+				</div>
+				<div className='center'>
+					<div className='center' className='burncoinerr3'>
+						You need 1000 interaction coins to activate this.
 					</div>
 					<div className='center'>
-						<div className='center' className='burncoinerr3'>
-							You need 1000 interaction coins to activate this.
-						</div>
-						<div className='center'>
-							<Link to='/'>
-								<a class='btn waves-effect waves-teal burn-err-button'>Earn Coins</a>
-							</Link>
-						</div>
+						<Link to='/'>
+							<a class='btn waves-effect waves-teal burn-err-button'>Earn Coins</a>
+						</Link>
 					</div>
 				</div>
 			</div>
