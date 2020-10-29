c615f4549e259eeb4deff91c3b38921708c44f06
ahtrahdis7 <sidharthabiki@gmail.com>
2020-10-29 21:56:57 +0530

    added filter to internships

--- src/Components/internshiplist.js
+++ src/Components/internshiplist.js
@@ -2,32 +2,52 @@ import React from 'react';
 import FadingLoader from './FadingLoader';
 import JobsandInternshipcard from './JobsandInternshipcard';
 
-const Internshiplist = (props) => {
-	const showInternships = props.internship.length ? (
-		props.internship.map((internship, index) => {
-			if (!/^https?:\/\//.test(internship.jobLink)) {
-				let weblink = 'https://' + internship.jobLink;
-				return <JobsandInternshipcard key={index} jobdetails={internship} weblink={weblink} />;
-			} else return <JobsandInternshipcard key={index} jobdetails={internship} weblink={internship.jobLink} />;
-		})
-	) : (
-		<div className='center'> Loading... </div>
-	);
-	return (
-		<div>
-			{props.internship[0] && (
-				<div>
-					<ul className='collection job-container' style={{ borderRadius: '7px' }}>
-						{showInternships}
-					</ul>
-				</div>
-			)}
-			{props.internship.length === 0 && (
-				<div>
-					<FadingLoader loadno={3} />
-				</div>
-			)}
-		</div>
-	);
+class Internshiplist extends React.Component {
+	
+	constructor (props) {
+		super(props);
+		this.filterJobs = this.filterJobs.bind(this);
+	}
+
+	filterJobs = (job) => {
+		if (this.props.filter === '') {
+			return true;
+		} else {
+			return this.props.filter === job.industry ? true : false;
+		}
+	};
+
+	render(){
+
+		var internships = this.props.internships.slice(0).reverse();
+		internships = internships.filter(this.filterJobs);
+
+		const showInternships = internships.length ? (
+			internships.map((internship, index) => {
+				if (!/^https?:\/\//.test(internship.jobLink)) {
+					let weblink = 'https://' + internship.jobLink;
+					return <JobsandInternshipcard key={index} jobdetails={internship} weblink={weblink} />;
+				} else return <JobsandInternshipcard key={index} jobdetails={internship} weblink={internship.jobLink} />;
+			})
+		) : (
+			<div className='center'> Oops! No Jobs To Show !! </div>
+		);
+		return (
+			<div>
+				{this.props.internships[0] && (
+					<div>
+						<ul className='collection job-container' style={{ borderRadius: '7px' }}>
+							{showInternships}
+						</ul>
+					</div>
+				)}
+				{this.props.internships.length === 0 && (
+					<div style={{textAlign: 'center'}}>
+						<FadingLoader loadno={3} />
+					</div>
+				)}
+			</div>
+		);
+	}
 };
 export default Internshiplist;
