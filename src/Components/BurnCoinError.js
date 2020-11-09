b29b18e739f3c663c2fa204a9c03213ffa768105
Nutella Enthusiast <ritika.iyer2000@gmail.com>
2020-11-09 18:30:44 +0530

    home page update

--- src/Components/BurnCoinError.js
+++ src/Components/BurnCoinError.js
@@ -1,39 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const BurnCoinError = (props) => {
-	return (
-		<div className='row' style={{ margin: '0' }}>
-			<div className='col l6 s12 divider-popup'>
-				<div className='center'>
-					<img
-						width='80%'
-						src={
-							'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1603256319/Theme/9_Icon_qdgix7.png'
-						}
-					/>
-				</div>
-			</div>
-			<div className='col l6 s12' style={{ paddingTop: '15px' }}>
-				<div className='center' className='burncoinerr1'>
-					997 MG users are in the waiting list!
-				</div>
-				<div className='center' className='burncoinerr1'>
+  return (
+    <div className="row" style={{ margin: "0" }}>
+      <div className="col l6 s12 divider-popup">
+        <div className="center">
+          <img
+            width="80%"
+            src={
+              "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1603256319/Theme/9_Icon_qdgix7.png"
+            }
+          />
+        </div>
+      </div>
+      <div className="col l6 s12" style={{ paddingTop: "15px" }}>
+        <div className="center" className="burncoinerr1">
+          500+ MG users are in the waiting list!
+        </div>
+        {/*<div className='center' className='burncoinerr1'>
 					Burn your interaction points to join the exclusive discounts on the products.
-				</div>
-				<div className='center'>
-					<div className='center' className='burncoinerr3'>
-						You need 1000 interaction coins to activate this.
-					</div>
-					<div className='center'>
-						<Link to='/'>
-							<a class='btn waves-effect waves-teal burn-err-button'>Earn Coins</a>
-						</Link>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+				</div>*/}
+        <div className="center">
+          <div className="center" className="burncoinerr3">
+            You need 1000 interaction coins to activate this.
+          </div>
+          <div className="center" className="burncoinerr1">
+            Glow more bulbs to earn
+          </div>
+          <div className="center">
+            <Link to="/">
+              <a class="btn waves-effect waves-teal burn-err-button">
+                Earn Coins
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BurnCoinError;
