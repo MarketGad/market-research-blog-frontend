72e0c76cfa0da4d1c78ce55837b5d11483e6e299
Nutella Enthusiast <ritika.iyer2000@gmail.com>
2020-12-05 15:35:14 +0530

    research cards fix

--- src/Screens/Industry.js
+++ src/Screens/Industry.js
@@ -1,127 +1,138 @@
-import React from 'react';
-import Footer from '../Components/Footer';
+import React from "react";
+import Footer from "../Components/Footer";
 
 const Industry = () => {
-	const articles = [
-		{
-			postName: 'Logistics Sector In India',
-			postImage:
-				'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1594916046/Articles%20Photos/Industry/Logistic%20Sector%20of%20India/1-min_ft40ju.jpg',
-			key: '1',
-			postBrief:
-				"India's logistics cost is estimated to be 13-14 % of GDP. In contrast,in developed economies like Germany, Sweden, Belgium, Austria, Japan and the USA, the cost varies between 9-10%. This article by Bisleshana(IIMI) give you a thorough study of India's Logistic Sector.",
-			postLink: '/i1001'
-		},
-		{
-			postName: 'Reverse Logistics',
-			postImage:
-				'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1594917488/Articles%20Photos/Industry/Reverse%20Logistic/2_k8fndm.jpg',
-			key: '2',
-			postBrief:
-				'The idea of recapturing value is the soul of this concept of reverse logistics. The value can be regained through reparation or refurbishment when returned to the market place, components from product returns can be reused as refurbished components or as spare parts',
-			postLink: '/i1002'
-		},
-		{
-			postName: 'Data Website',
-			postImage:
-				'https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80',
-			key: '3',
-			postBrief:
-				'Given the present market scenario, India stands at a very competitive position in the field. It is among the Top 10 countries dealing with big data. India has more than 600 firms among which 50% being startups provides the country an edge over others.',
-			postLink: '/i1003'
-		},
-		{
-			postName: 'FinTech Apps',
-			postImage:
-				'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1594919224/Articles%20Photos/Industry/FinTech/2_2_-min_whkytt.jpg',
-			key: '4',
-			postBrief:
-				"India ranks amongst the world's fastest growing FinTech markets. India rated FinTech's adoption rate, with China, at the top in the world. A 20 per cent CAGR is expected to increase the volume of digital payments by $65 billion by 2023 in 2019.",
-			postLink: '/i1004'
-		},
-		{
-			postName: 'Education Industry and Covid-19',
-			postImage:
-				'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1594921120/Articles%20Photos/Industry/Covid19%20on%20Education%20Industry/2_3_-min_squcan.jpg',
-			key: '5',
-			postBrief:
-				'Instead of shutting schools in at least 188 countries, over 91% of children around the world are now out of school. The education sector faces unparalleled challenges and needs to change to keep children engaged.',
-			postLink: '/i1005'
-		},
-		{
-			postName: '12 Steps to Start a Successful Business',
-			postImage:
-				'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1595774051/Articles%20Photos/Industry/12%20Steps%20to%20Start%20a%20Successful%20Business%20Startup/1_cubx34.jpg',
-			key: '6',
-			postBrief:
-				'Bypassing some steps or thinking that one or more of the 12 steps below might not apply to your startup is how many companies failed or invested resources they didn’t need to move forward.',
-			postLink: '/i1006'
-		},
-		{
-			postName: 'Impact Of Covid-19 On Logistics Sector In India',
-			postImage:
-				'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1597767828/Articles%20Photos/Industry/Logistics%20Sector%20Of%20India%20Part%20II/1_1_nybaqx.jpg',
-			key: '7',
-			postBrief:
-				'Since   the   3rd   quarter   of   2019,   the   Indian   economy   is   facing   turbulences   due   to   falling manufacturing/industrial   production,   especially   in   auto   and   ancillary   sector,   rising   non-performing assets (NPAs)...',
-			postLink: '/i1007'
-		},
-		{
-			postName: 'Opportunities In Warehousing Technologies In India',
-			postImage:
-				'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1599417289/Articles%20Photos/Industry/Warehousing%20Technologies/1_xob8wh.jpg',
-			key: '8',
-			postBrief:
-				'Earlier the Indian warehousing industry, which was dominated by unorganised players,is slowly see increased participation from organised players.',
-			postLink: '/i1008'
-		},
-		{
-			postName: 'Impact of Covid 19 on Agriculture Sector In India',
-			postImage:
-				'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1603447653/Articles%20Photos/Industry/farmers-759_xkoi6v.jpg',
-			key: '9',
-			postBrief:
-				"Agriculture contributes over 17% of India's GDP. Agriculture is the primary source oflivelihoods in India with its allied industries. For their livelihoods, 70 percent of ruralhouseholds still rely mainly on agriculture.",
-			postLink: '/i1009'
-		}
-	];
-	const showArticles = articles.length ? (
-		articles.map((article) => {
-			return (
-				<div className='col l6 s12'>
-					<div className='card card-height'>
-						<div className='card-content content-research'>
-							<div className='row' style={{ margin: '0' }}>
-								<div className='col s12' style={{ padding: '0' }}>
-									<img pos className='post-image' src={article.postImage} alt='1st' />
-								</div>
-								<div className='col s12'>
-									<h5 className='landing-post-head'> {article.postName} </h5>
-									<p className='post-brief'> {article.postBrief} </p>
-									<div className='center'>
-										<a href={article.postLink} className='btn waves-effect waves-light read-more'>
-											Read More
-										</a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			);
-		})
-	) : (
-		<div className='center'> No articles to show: ( </div>
-	);
+  const articles = [
+    {
+      postName: "Logistics Sector In India",
+      postImage:
+        "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1594916046/Articles%20Photos/Industry/Logistic%20Sector%20of%20India/1-min_ft40ju.jpg",
+      key: "1",
+      postBrief:
+        "India's logistics cost is estimated to be 13-14 % of GDP. In contrast,in developed economies like Germany, Sweden, Belgium, Austria, Japan and the USA, the cost varies between 9-10%. This article by Bisleshana(IIMI) give you a thorough study of India's Logistic Sector.",
+      postLink: "/i1001",
+    },
+    {
+      postName: "Reverse Logistics",
+      postImage:
+        "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1594917488/Articles%20Photos/Industry/Reverse%20Logistic/2_k8fndm.jpg",
+      key: "2",
+      postBrief:
+        "The idea of recapturing value is the soul of this concept of reverse logistics. The value can be regained through reparation or refurbishment when returned to the market place, components from product returns can be reused as refurbished components or as spare parts",
+      postLink: "/i1002",
+    },
+    {
+      postName: "Data Website",
+      postImage:
+        "https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80",
+      key: "3",
+      postBrief:
+        "Given the present market scenario, India stands at a very competitive position in the field. It is among the Top 10 countries dealing with big data. India has more than 600 firms among which 50% being startups provides the country an edge over others.",
+      postLink: "/i1003",
+    },
+    {
+      postName: "FinTech Apps",
+      postImage:
+        "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1594919224/Articles%20Photos/Industry/FinTech/2_2_-min_whkytt.jpg",
+      key: "4",
+      postBrief:
+        "India ranks amongst the world's fastest growing FinTech markets. India rated FinTech's adoption rate, with China, at the top in the world. A 20 per cent CAGR is expected to increase the volume of digital payments by $65 billion by 2023 in 2019.",
+      postLink: "/i1004",
+    },
+    {
+      postName: "Education Industry and Covid-19",
+      postImage:
+        "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1594921120/Articles%20Photos/Industry/Covid19%20on%20Education%20Industry/2_3_-min_squcan.jpg",
+      key: "5",
+      postBrief:
+        "Instead of shutting schools in at least 188 countries, over 91% of children around the world are now out of school. The education sector faces unparalleled challenges and needs to change to keep children engaged.",
+      postLink: "/i1005",
+    },
+    {
+      postName: "12 Steps to Start a Successful Business",
+      postImage:
+        "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1595774051/Articles%20Photos/Industry/12%20Steps%20to%20Start%20a%20Successful%20Business%20Startup/1_cubx34.jpg",
+      key: "6",
+      postBrief:
+        "Bypassing some steps or thinking that one or more of the 12 steps below might not apply to your startup is how many companies failed or invested resources they didn’t need to move forward.",
+      postLink: "/i1006",
+    },
+    {
+      postName: "Impact Of Covid-19 On Logistics Sector In India",
+      postImage:
+        "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1597767828/Articles%20Photos/Industry/Logistics%20Sector%20Of%20India%20Part%20II/1_1_nybaqx.jpg",
+      key: "7",
+      postBrief:
+        "Since   the   3rd   quarter   of   2019,   the   Indian   economy   is   facing   turbulences   due   to   falling manufacturing/industrial   production,   especially   in   auto   and   ancillary   sector,   rising   non-performing assets (NPAs)...",
+      postLink: "/i1007",
+    },
+    {
+      postName: "Opportunities In Warehousing Technologies In India",
+      postImage:
+        "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1599417289/Articles%20Photos/Industry/Warehousing%20Technologies/1_xob8wh.jpg",
+      key: "8",
+      postBrief:
+        "Earlier the Indian warehousing industry, which was dominated by unorganised players,is slowly see increased participation from organised players.",
+      postLink: "/i1008",
+    },
+    {
+      postName: "Impact of Covid 19 on Agriculture Sector In India",
+      postImage:
+        "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1603447653/Articles%20Photos/Industry/farmers-759_xkoi6v.jpg",
+      key: "9",
+      postBrief:
+        "Agriculture contributes over 17% of India's GDP. Agriculture is the primary source oflivelihoods in India with its allied industries. For their livelihoods, 70 percent of ruralhouseholds still rely mainly on agriculture.",
+      postLink: "/i1009",
+    },
+  ];
+  const showArticles = articles.length ? (
+    articles.map((article) => {
+      return (
+        <div className="col l6 s12">
+          <div
+            className="card card-height"
+            style={{ height: "auto", minHeight: "700px" }}
+          >
+            <div className="card-content content-research">
+              <div className="row" style={{ margin: "0" }}>
+                <div className="col s12" style={{ padding: "0" }}>
+                  <img
+                    pos
+                    className="post-image"
+                    src={article.postImage}
+                    alt="1st"
+                  />
+                </div>
+                <div className="col s12">
+                  <h5 className="landing-post-head"> {article.postName} </h5>
+                  <p className="post-brief"> {article.postBrief} </p>
+                  <div className="center">
+                    <a
+                      href={article.postLink}
+                      className="btn waves-effect waves-light read-more"
+                    >
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    })
+  ) : (
+    <div className="center"> No articles to show: ( </div>
+  );
 
-	return (
-		<div>
-			<div className='post-container'>
-				<div className='row'>{showArticles}</div>
-			</div>
-			<Footer />
-		</div>
-	);
+  return (
+    <div>
+      <div className="post-container">
+        <div className="row">{showArticles}</div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Industry;
--- src/Screens/VentureHack.js
+++ src/Screens/VentureHack.js
@@ -1,89 +1,100 @@
-import React from 'react';
-import Footer from '../Components/Footer';
+import React from "react";
+import Footer from "../Components/Footer";
 
 const VentureHack = () => {
-	const articles = [
-		{
-			postName: 'Tradohub: Bootstrapped To Build The Largest SME Ecosystem',
-			postImage:
-				'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1594928729/Articles%20Photos/VentureHack/SME-social_fshval.jpg',
-			key: '1',
-			postBrief:
-				'The company has targeted to increase its turnover over 20 fold to $1 billion from existing Rs 330 crore in the next five years by 2025...',
-			postLink: '/v3001'
-		},
-		{
-			postName: 'First Indian Handcrafted Beer- Bira91',
-			postImage:
-				'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1595775393/Articles%20Photos/VentureHack/First%20Indian%20Handcrafted%20Beer%20Bira91/Slide6_fwxubr.jpg',
-			key: '2',
-			postBrief:
-				'Bira91 is founded by Ankur Jain, a Computer Science graduate from University of Chicago, who previously had a healthcare startup in New York. There he came across the concept of craft beers, originated in the European countries.',
-			postLink: '/v3002'
-		},
-		{
-			postName: 'Unacademy, India’s largest EdTech Company',
-			postImage:
-				'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1595775916/Articles%20Photos/VentureHack/Unacademy/Slide1_vdxojs.jpg',
-			key: '3',
-			postBrief:
-				'Unacademy was founded by Gaurav Munjal in 2010, who was later on accompanied by Hemesh Singh and Roman Saini. Gaurav Munjal started his Unacademy journey way before the other two. He started the YouTube channel named Unacademy.',
-			postLink: '/v3003'
-		},
-		{
-			postName: '12 Steps to Start a Successful Business',
-			postImage:
-				'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1594928817/Articles%20Photos/VentureHack/startuptop_kzuhdz.jpg',
-			key: '4',
-			postBrief:
-				' Bypassing some steps or thinking that one or more of the 12 steps below might not apply to your startup is how many companies failed or invested resources they didn’t need to move forward.Identify in which step you are and continue down the path.',
-			postLink: '/v3004'
-		},
-		{
-			postName: 'DESIGNING A BUSINESS MODEL CANVAS',
-			postImage:
-				'https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1601711487/Articles%20Photos/VentureHack/1_gr9tkq.jpg',
-			key: '5',
-			postBrief:
-				'The Business Model Canvas is a project strategy framework for the implementation of new business models or for documenting the existing ones. It is a visual chart with elements which represent the clients, value proposition, finances and infrastructure of a business or product.',
-			postLink: '/v3005'
-		}
-	];
-	const showArticles = articles.length ? (
-		articles.map((article) => {
-			return (
-				<div className='col l6 s12'>
-					<div className='card card-height'>
-						<div className='card-content content-research'>
-							<div className='row' style={{ margin: '0' }}>
-								<div className='col s12' style={{ padding: 0 }}>
-									<img pos className='post-image' src={article.postImage} alt='1st' />
-								</div>
-								<div className='col s12'>
-									<h5 className='landing-post-head'> {article.postName} </h5>
-									<p className='post-brief'> {article.postBrief} </p>
-									<div className='center'>
-										<a href={article.postLink} className='btn waves-effect waves-light read-more'>
-											Read More
-										</a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			);
-		})
-	) : (
-		<div className='center'>No articles to show :(</div>
-	);
+  const articles = [
+    {
+      postName: "Tradohub: Bootstrapped To Build The Largest SME Ecosystem",
+      postImage:
+        "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1594928729/Articles%20Photos/VentureHack/SME-social_fshval.jpg",
+      key: "1",
+      postBrief:
+        "The company has targeted to increase its turnover over 20 fold to $1 billion from existing Rs 330 crore in the next five years by 2025...",
+      postLink: "/v3001",
+    },
+    {
+      postName: "First Indian Handcrafted Beer- Bira91",
+      postImage:
+        "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1595775393/Articles%20Photos/VentureHack/First%20Indian%20Handcrafted%20Beer%20Bira91/Slide6_fwxubr.jpg",
+      key: "2",
+      postBrief:
+        "Bira91 is founded by Ankur Jain, a Computer Science graduate from University of Chicago, who previously had a healthcare startup in New York. There he came across the concept of craft beers, originated in the European countries.",
+      postLink: "/v3002",
+    },
+    {
+      postName: "Unacademy, India’s largest EdTech Company",
+      postImage:
+        "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1595775916/Articles%20Photos/VentureHack/Unacademy/Slide1_vdxojs.jpg",
+      key: "3",
+      postBrief:
+        "Unacademy was founded by Gaurav Munjal in 2010, who was later on accompanied by Hemesh Singh and Roman Saini. Gaurav Munjal started his Unacademy journey way before the other two. He started the YouTube channel named Unacademy.",
+      postLink: "/v3003",
+    },
+    {
+      postName: "12 Steps to Start a Successful Business",
+      postImage:
+        "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1594928817/Articles%20Photos/VentureHack/startuptop_kzuhdz.jpg",
+      key: "4",
+      postBrief:
+        " Bypassing some steps or thinking that one or more of the 12 steps below might not apply to your startup is how many companies failed or invested resources they didn’t need to move forward.Identify in which step you are and continue down the path.",
+      postLink: "/v3004",
+    },
+    {
+      postName: "DESIGNING A BUSINESS MODEL CANVAS",
+      postImage:
+        "https://res.cloudinary.com/marketgaddevcloud1/image/upload/v1601711487/Articles%20Photos/VentureHack/1_gr9tkq.jpg",
+      key: "5",
+      postBrief:
+        "The Business Model Canvas is a project strategy framework for the implementation of new business models or for documenting the existing ones. It is a visual chart with elements which represent the clients, value proposition, finances and infrastructure of a business or product.",
+      postLink: "/v3005",
+    },
+  ];
+  const showArticles = articles.length ? (
+    articles.map((article) => {
+      return (
+        <div className="col l6 s12">
+          <div
+            className="card card-height"
+            style={{ height: "auto", minHeight: "700px" }}
+          >
+            <div className="card-content content-research">
+              <div className="row" style={{ margin: "0" }}>
+                <div className="col s12" style={{ padding: 0 }}>
+                  <img
+                    pos
+                    className="post-image"
+                    src={article.postImage}
+                    alt="1st"
+                  />
+                </div>
+                <div className="col s12">
+                  <h5 className="landing-post-head"> {article.postName} </h5>
+                  <p className="post-brief"> {article.postBrief} </p>
+                  <div className="center">
+                    <a
+                      href={article.postLink}
+                      className="btn waves-effect waves-light read-more"
+                    >
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    })
+  ) : (
+    <div className="center">No articles to show :(</div>
+  );
 
-	return (
-		<div>
-			<div className='row post-container'>{showArticles}</div>
-			<Footer />
-		</div>
-	);
+  return (
+    <div>
+      <div className="row post-container">{showArticles}</div>
+      <Footer />
+    </div>
+  );
 };
 
 export default VentureHack;
